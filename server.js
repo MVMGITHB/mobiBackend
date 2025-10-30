9c9805e52bfddd78ab938480aa0f246dd356b80e
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-10-30 16:44:21 +0530

    cors allow

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ const app = express();
 const allowedOrigins = new Set([
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://admin-daily-news.vercel.app",
-  "https://admin-daily-news.vercel.app/",
+  "https://mobi-admin.vercel.app/",
+  "https://mobi-admin.vercel.app",
   "http://82.25.109.68:3005",
   "http://82.25.109.68:3005/",
-  "https://dailynewzmail.com/",
-  "https://dailynewzmail.com"
+  "https://mobiperform.com/",
+  "https://mobiperform.com"
 
 ]);
 
